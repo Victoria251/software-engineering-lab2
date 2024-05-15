73dffd46b43cff949d617ff88e1a1d15fc466773
Victoria251 <vikahrapolovic@gmail.com>
2024-05-15 18:45:01 +0300

    Done.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -120,7 +120,7 @@ const config = {
             items: [
               {
                 //label: 'Blog',
-                to: '/blog',
+                //to: '/blog',
               },
               {
                 label: 'GitHub',
